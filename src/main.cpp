ceb88fa21e5d74f795f00787a8434d710c4c3170
Sohil Zidan <zidansohil@gmail.com>
2020-11-07 18:03:29 +0100

    dynamic programming v1

---
+++ src/main.cpp
@@ -0,0 +1,395 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string> 
+#include <fstream>
+#include <sstream>
+#include "main.h"
+
+int main(int argc, char** argv) {
+
+  ////////////////
+  // Parameters //
+  ////////////////
+
+  // camera setup parameters
+  const double focal_length = 1247;
+  const double baseline = 213;
+
+  // stereo estimation parameters
+  const int dmin = 67;
+  const int window_size = 3;
+  const double weight = 500;
+  const double scale = 3;
+
+  ///////////////////////////
+  // Commandline arguments //
+  ///////////////////////////
+
+  if (argc < 4) {
+    std::cerr << "Usage: " << argv[0] << " IMAGE1 IMAGE2 OUTPUT_FILE" << std::endl;
+    return 1;
+  }
+
+  cv::Mat image1 = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  cv::Mat image2 = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
+  const std::string output_file = argv[3];
+
+
+  
+
+  if (!image1.data) {
+    std::cerr << "No image1 data" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (!image2.data) {
+    std::cerr << "No image2 data" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //// showing left image
+  //cv::namedWindow("left", cv::WINDOW_AUTOSIZE);
+  //cv::imshow("left", image1);
+  //// showing right image
+  //cv::namedWindow("right", cv::WINDOW_AUTOSIZE);
+  //cv::imshow("right", image2);
+
+  std::cout << "------------------ Parameters -------------------" << std::endl;
+  std::cout << "focal_length = " << focal_length << std::endl;
+  std::cout << "baseline = " << baseline << std::endl;
+  std::cout << "window_size = " << window_size << std::endl;
+  std::cout << "occlusion weights = " << weight << std::endl;
+  std::cout << "disparity added due to image cropping = " << dmin << std::endl;
+  std::cout << "scaling of disparity images to show = " << scale << std::endl;
+  std::cout << "output filename = " << argv[3] << std::endl;
+  std::cout << "-------------------------------------------------" << std::endl;
+
+  int height = image1.size().height;
+  int width = image1.size().width;
+
+  ////////////////////
+  // Reconstruction //
+  ////////////////////
+
+  // Naive disparity image
+  //cv::Mat naive_disparities = cv::Mat::zeros(height - window_size, width - window_size, CV_8UC1);
+  cv::Mat naive_disparities = cv::Mat::zeros(height, width, CV_8UC1);
+  cv::Mat disparities = cv::Mat::zeros(height, width, CV_8UC1);
+
+  /*StereoEstimation_Naive(
+    window_size, dmin, height, width,
+    image1, image2,
+    naive_disparities, scale);*/
+  //image1 /= 255; 
+  //image2 /= 255;
+  //image1.convertTo(image1, CV_32F, 1.0 / 255, 0);
+  //image2.convertTo(image2, CV_32F, 1.0 / 255, 0);
+
+  /*double minVal;
+  double maxVal;
+  cv::Point minLoc;
+  cv::Point maxLoc;
+
+  cv::minMaxLoc(image1, &minVal, &maxVal, &minLoc, &maxLoc);
+  
+  std::cout << maxVal << std::endl;
+  std::cin.get();
+  */
+  StereoEstimation_DP(
+      window_size,
+      height,
+      width,
+      image1, image2,
+      disparities, scale
+  );
+
+  ////////////
+  // Output //
+  ////////////
+
+  // reconstruction
+  Disparity2PointCloud(
+    output_file,
+    height, width, naive_disparities,
+    window_size, dmin, baseline, focal_length);
+
+  // save / display images
+  std::stringstream out1;
+  out1 << output_file << "_naive.png";
+  cv::imwrite(out1.str(), disparities);
+
+  cv::namedWindow("Disparity", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Disparity", disparities);
+
+  cv::waitKey(0);
+
+  return 0;
+}
+
+void StereoEstimation_DP(
+    const int& window_size,
+    int height,
+    int width,
+    cv::Mat& image1, cv::Mat& image2,
+    cv::Mat& disparities, const double& scale
+) 
+{
+    int half_window_size = window_size / 2;
+    cv::Mat C;// = cv::Mat::zeros(width, width, CV_8UC1);
+    cv::Mat M;// = cv::Mat::ones(cv::Size(width, width), CV_8UC1);
+
+    cv::Mat disleft = cv::Mat::zeros(height, width, CV_8UC1);
+    cv::Mat disright = cv::Mat::zeros(height, width, CV_8UC1);
+    float lambda = 500; // occlusion cost
+
+//#pragma omp parallel for
+    for (int row = 0; row < height; ++row) {
+        cv::Mat C = cv::Mat::zeros(width, width, CV_8UC1);
+        cv::Mat M = cv::Mat::ones(width, width, CV_8UC1);
+        std::cout
+            << "Calculating disparities for the DP approach... "
+            << std::ceil((row / static_cast<double>(height - 1)) * 100) << "%\r"
+            << std::flush;
+           
+
+        // initialization
+        for (int k = 1; k < width/* - half_window_size*/; k++)
+        {
+            C.at<uchar>(k, 0) = k * lambda;
+        }
+        for (int k = 1; k < width/* - half_window_size*/; k++)
+        {
+            C.at<uchar>(0, k) = k * lambda;
+        }
+
+
+        for (int i = 1; i < width; i++)
+        {
+            for (int j = 1; j < width; j++)
+            {
+                int dissimilarity = 0;
+                /*for (int u = -half_window_size; u <= half_window_size; u++)
+                {
+                    for (int v = -half_window_size; v <= half_window_size; v++)
+                    {
+                        int val_left = image1.at<uchar>(row + u, i + v);
+                        int val_right = image2.at<uchar>(row + u, j + v);
+                        dissimilarity += (val_left - val_right) * (val_left - val_right);
+                    }
+
+                }*/
+                int val_left = image1.at<uchar>(row, i);
+                int val_right = image2.at<uchar>(row, j);
+                dissimilarity = (val_left - val_right) * (val_left - val_right);
+
+                float min1 = C.at<uchar>(i - 1, j - 1) + dissimilarity; // match
+                float min2 = C.at<uchar>(i - 1, j) + lambda; // left occlusion
+                float min3 = C.at<uchar>(i, j - 1) + lambda; // right occlusion
+                
+                // finding optimal cost 
+                float min_c = std::min(min1, std::min(min2, min3));
+                C.at<uchar>(i, j) = min_c;
+
+                float eps = 1.0e-05;
+                if (fabs( min_c - min1) <= eps) {
+                    M.at<uchar>(i, j) = 1; // match
+                }
+                
+                else if (fabs(min_c - min2) <= eps) {
+                    M.at<uchar>(i, j) = 2; // occluded from left
+                }
+                else if (fabs(min_c - min3) <= eps) 
+                {
+                    M.at<uchar>(i, j) = 3; // occluded from right
+                }
+                
+            }
+
+        }
+        /*std::cin.get();
+        cv::namedWindow("Left Disparity", cv::WINDOW_AUTOSIZE);
+        cv::imshow("Left Disparity", C);
+        break;*/
+        //std::cout << "Computing disparity" << std::endl;
+        int i = width - 1;
+        int j = i;
+        while (i > 0 && j > 0)
+        {
+            switch (M.at<uchar>(i, j))
+            {
+            case 1:
+                disleft.at<uchar>(row, i) = abs(i - j) * scale;
+                disright.at<uchar>(row, j) = abs(j - i) * scale;
+                i --;
+                j--;
+                break;
+            case 2:
+                disleft.at<uchar>(row, i) = 0;
+                i--;
+                break;
+            case 3:
+                disright.at<uchar>(row, j) = 0;
+                j--;
+                break;
+            }
+            
+        }
+        /*if (row == 10)
+        {
+            cv::namedWindow("Disimilarity", cv::WINDOW_AUTOSIZE);
+            cv::imshow("Disimilarity", C);
+            break;
+        }*/
+    }
+
+    cv::namedWindow("Left Disparity", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Left Disparity", disleft);
+
+    cv::namedWindow("Right Disparity", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Right Disparity", disright);
+    disparities = disleft;
+    //std::cout << disparities;
+}
+
+void StereoEstimation_Naive(
+  const int& window_size,
+  const int& dmin,
+  int height,
+  int width,
+  cv::Mat& image1, cv::Mat& image2, cv::Mat& naive_disparities, const double& scale)
+{
+  int half_window_size = window_size / 2;
+  int dims[3] = { height, width, width };
+  // store the patch window result of each coordinate with each disparity value to use is with box filtering
+  cv::Mat box_filter = cv::Mat::zeros(3, dims, CV_32F);
+  // false at the first row scan
+  bool first_row_skipped = false;
+
+  for (int i = half_window_size; i < height - half_window_size; ++i) {
+
+    std::cout
+      << "Calculating disparities for the naive approach... "
+      << std::ceil(((i - half_window_size + 1) / static_cast<double>(height - window_size + 1)) * 100) << "%\r"
+      << std::flush;
+
+//#pragma omp parallel for
+    for (int j = half_window_size; j < width - half_window_size; ++j) {
+      int min_ssd = INT_MAX;
+      int disparity = 0;
+
+      for (int d = -j + half_window_size; d < width - j - half_window_size; ++d) {
+          int ssd = 0;
+
+          // TODO: sum up matching cost (ssd) in a window
+          if (!first_row_skipped || true)
+          {
+              for (int u = -half_window_size; u <= half_window_size; u++)
+              {
+                  for (int v = -half_window_size; v <= half_window_size; v++)
+                  {
+                      int val_left = image1.at<uchar>(i + u, j + v);
+                      int val_right = image2.at<uchar>(i + u, j + v + d);
+                      ssd += (val_left - val_right) * (val_left - val_right);
+                  }
+
+              }
+              
+          }
+          else
+          {
+              // ssd of x,y-1
+              /*ssd = box_filter.at<uchar>(i- 1, j, d);
+              std::cout << "ssd = " << ssd << std::endl;
+              std::cout << i - half_window_size - 1 << ", " << j - half_window_size << ", " << d << std::endl;
+              std::cout << "i = " << i << ", j = " << j <<  std::endl;*/
+              int ss;
+              
+
+              // green part
+              int included_row = 0;
+              //std::cout << "Including indices:" << std::endl;
+              for (int k = -half_window_size; k <= half_window_size; k++)
+              {
+                  //std::cout << "(" << i + half_window_size << ", " << j + k << ")" << ", ";
+                  int val_left = image1.at<uchar>(i + half_window_size, j + k);
+                  int val_right = image2.at<uchar>(i + half_window_size, j + k + d);
+                  included_row += (val_left - val_right) * (val_left - val_right);
+              }
+              //std::cout << std::endl;
+
+              // red part
+              int excluded_row = 0;
+              //std::cout << "Excluding indices:" << std::endl;
+              for (int k = -half_window_size; k <= half_window_size; k++)
+              {
+                  //std::cout << "(" << i - half_window_size - 1 << ", " << j + k << ")" << ", ";
+                  int val_left = image1.at<uchar>(i - half_window_size - 1, j + k);
+                  int val_right = image2.at<uchar>(i - half_window_size - 1, j + k + d);
+                  excluded_row += (val_left - val_right) * (val_left - val_right);
+              }
+
+              //std::cin.get();
+              //ssd = prev_ssd;
+              /*std::cout << "included = " << included_row << std::endl;
+              std::cout << "excluded = " << excluded_row << std::endl;
+              ssd += included_row - excluded_row;
+              std::cout << "new ssd = " << ssd << std::endl;
+              std::cout << "--------------------------------" <<std::endl;*/
+              // red: i - half_window_size - 1, x= j + (-half_window_size --> half_window_size)
+              // green: i + n, x= j + (-half_window_size --> half_window_size)
+          }
+
+          // box_filter.at<uchar>(i, j, d) = ssd;
+          /*if (d == 14) {
+              std::cout << std::endl;
+              std::cout << ssd << std::endl;
+              std::cout << i << std::endl;
+              std::cout << j << std::endl;
+              std::cout << d << std::endl;
+              std::cout << i - half_window_size << std::endl;
+              std::cout << j - half_window_size << std::endl;
+              std::cout << box_filter.at<uchar>(i, j, d) << std::endl;
+              std::cin.get();
+          }*/
+
+          if (ssd < min_ssd) {
+              min_ssd = ssd;
+              disparity = d;
+          }
+      }
+
+      naive_disparities.at<uchar>(i - half_window_size, j - half_window_size) = std::abs(disparity) * scale;
+    }
+    first_row_skipped = true;
+  }
+
+  std::cout << "Calculating disparities for the naive approach... Done.\r" << std::flush;
+  std::cout << std::endl;
+}
+
+void Disparity2PointCloud(
+  const std::string& output_file,
+  int height, int width, cv::Mat& disparities,
+  const int& window_size,
+  const int& dmin, const double& baseline, const double& focal_length)
+{
+  std::stringstream out3d;
+  out3d << output_file << ".xyz";
+  std::ofstream outfile(out3d.str());
+  for (int i = 0; i < height - window_size; ++i) {
+    std::cout << "Reconstructing 3D point cloud from disparities... " << std::ceil(((i) / static_cast<double>(height - window_size + 1)) * 100) << "%\r" << std::flush;
+    for (int j = 0; j < width - window_size; ++j) {
+      if (disparities.at<uchar>(i, j) == 0) continue;
+
+      // TODO
+      //const double Z = ...
+      //const double X = ...
+      //const double Y = ...
+	  //
+      //outfile << X << " " << Y << " " << Z << std::endl;
+    }
+  }
+
+  std::cout << "Reconstructing 3D point cloud from disparities... Done.\r" << std::flush;
+  std::cout << std::endl;
+}
